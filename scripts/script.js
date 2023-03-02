0a26fcda22abe7eb1540567fdabe52d6f7fc4313
AlexanderTabakov <sir.alexander.tabakov@gmail.com>
2023-03-02 14:20:45 +0700

    сделал дз5

--- scripts/script.js
+++ scripts/script.js
@@ -1,27 +1,59 @@
 "use strict";
 
-const money = 100000;
+const getRandomNumber = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
 
-const profit = 'Фриланс';
+const validateAnswer = (answer) => {
+    if (answer === '') alert('Нужно ввести хоть что-то');
+    else if (isNaN(answer)) alert('Введите число!');
+    else return true;
+}
 
-const expenses = 'Коммуналка, Питание, Бензин, Котик, Бары '
 
-const purpose = 1000000;
+const start = () => {
+    const MIN_VALUE = 1;
+    const MAX_VALUE = 10;
 
-const period = 3;
+    const ATTEMPTS_COUNT = 5;
 
-console.log(typeof money, typeof profit);
+    let attemptsLeft = ATTEMPTS_COUNT;
 
-console.log(expenses.length);
+    const searchNumber = getRandomNumber(MIN_VALUE, MAX_VALUE);
 
-console.log(`Период равен ${period}--м месяцем`);
+    const loop = () => {
+        if (attemptsLeft === 0) {
+            const runAgain = confirm('У вас закончились попытки, хотите сыграть еще?');
 
-console.log(`Цель заработать ${purpose} рублей`);
+            if (runAgain) start();
+            else alert('GAME OVER');
 
-const budgetPerDay = money / 30;
+            return;
+        }
 
-console.log(`${ Math.floor(budgetPerDay)}р / в день`);
+        const userAnswer = prompt(`Угадай число от ${MIN_VALUE} до ${MAX_VALUE} за ${attemptsLeft} попыток`);
 
-console.log(expenses.toLowerCase());
+        if (userAnswer === null) {
+            alert('GAME OVER');
+            return;
+        }
 
-console.log(expenses.split(`, `));
+        if (validateAnswer(userAnswer)) {
+            const answer = Number.parseInt(userAnswer);
+            if (answer > searchNumber) {
+                attemptsLeft--;
+                alert(`Загаднное число меньше, осталось ${attemptsLeft} попыток`);
+                loop();
+            } else if (answer < searchNumber) {
+                attemptsLeft--;
+                alert(`Загаднное число больше, осталось ${attemptsLeft} попыток`);
+                loop();
+            } else if (answer === searchNumber) {
+                const runAgain = confirm(`Вы угадали за ${ATTEMPTS_COUNT - attemptsLeft + 1} попыток, хотите сыграть еще?`);
+                if (runAgain) start();
+                else alert('GAME OVER')
+            }
+        } else loop();
+
+    }
+
+    loop();
+}
