da1a2aae543bffa476a42d1a88762c8520355cfd
AlexanderTabakov <sir.alexander.tabakov@gmail.com>
2023-03-10 15:17:52 +0700

    Сделал ДЗ 8

--- scripts/script.js
+++ scripts/script.js
@@ -1,23 +1,5 @@
 "use strict";
 
-// let purpose = Number.parseFloat(prompt('Желаемая сумма Вашего дохода'));
-// let salary = Number.parseFloat(prompt('Ваш месячный доход'));
-// let extraMoney  = Number.parseFloat(prompt('Укажите ваш дополнительный доход'));
-// let expensesList = Number.parseFloat(prompt('Перечислите возможные расходы зарассчитываемый период через запятую'));
-// let expences  = Number.parseFloat(prompt('Восколько обойдутся обязательные статьи расходов?'));
-//
-//
-//
-// let isDeposit = confirm('Есть ли у вас депозит в банке?');
-//
-// let profitFromDeposit;
-//
-// if (isDeposit === true) {
-//
-//     profitFromDeposit = Number.parseFloat(prompt('Введите мейсячный доход с депозита'));
-//
-// } else profitFromDeposit = 0;
-
 function print(text) {
     document.write(`<div>${ text }</div>`);
 }
@@ -28,32 +10,40 @@ const getTargetMonth = (accumulatedIncome, purpose) => Math.ceil(purpose / accum
 
 const getBudgetPerDay = (AccumulatedIncome) => Math.floor(AccumulatedIncome / 30);
 
+const questionLoop = (question, defaultValue) => {
+    const answer = Number.parseFloat(prompt(question, defaultValue));
+
+    if (isNaN(answer)) return questionLoop(question, defaultValue);
+    return answer;
+}
+
+
 const init = () => {
 
-    let salary = Number.parseFloat(prompt('Ваш месячный доход'));
+    let salary = questionLoop('Ваш месячный доход', '100000');
     print(`Месячный доход ${salary} р`);
 
-    let extraMoney = Number.parseFloat(prompt('Укажите ваш дополнительный доход'));
+    let extraMoney = questionLoop('Укажите ваш дополнительный доход', '50000');
     print(`Дополнительный доход ${extraMoney} р`);
 
     let isDeposit = confirm('Есть ли у вас депозит в банке?');
     let profitFromDeposit;
     if (isDeposit) {
 
-      profitFromDeposit = Number.parseFloat(prompt('Введите мейсячный доход с депозита'));
+      profitFromDeposit = questionLoop('Введите месячный доход с депозита', '10000');
         print(`Доход с депозита ${profitFromDeposit} р`);
 
 
     } else profitFromDeposit = 0;
 
 
-    let expensesList = Number.parseFloat(prompt('Перечислите возможные расходы зарассчитываемый период через запятую'));
-    print(`Категории расходов ${expensesList} р`);
+    let expensesList = prompt('Перечислите возможные расходы зарассчитываемый период через запятую', 'Питание, питие, бензин, котик, ништяки');
+    print(`Категории расходов ${expensesList}`);
 
-    let expences = Number.parseFloat(prompt('Восколько обойдутся обязательные статьи расходов?'));
+    let expences = questionLoop('Восколько обойдутся обязательные статьи расходов?', '50000');
     print(`Сумма расходов ${expences} р`);
 
-    let purpose = Number.parseFloat(prompt('Желаемая сумма Вашего дохода'));
+    let purpose = questionLoop('Желаемая сумма Вашего дохода', '1000000');
     print(`Целевая сумма ${purpose} р`);
 
     const accumulatedIncome = getAccumulatedIncome(salary, extraMoney, expences, profitFromDeposit);
@@ -65,11 +55,10 @@ const init = () => {
     let budgetPerMonth = Math.floor(accumulatedIncome / 30);
     print(`Бюджет на на месяц ${budgetPerMonth}`);
 
-    // let gainTime = Math.ceil(purpose / budgetPerMonth);
-
-    // alert(`Вы достигнете желаемого дохода через ${gainTime} месяцев`);
-
-    // let budgetPerDay = Math.floor(budgetPerMonth / 30);
+    if (targetMonth <=0 ) {
+        print('Цель не будет достигнута');
+    }
+    else print('Цель будет достигнута')
 
     let budgetPerDay = Math.floor(accumulatedIncome / 30);
 
@@ -86,4 +75,22 @@ const init = () => {
 
 }
 
+const array = ['243242342', '2432342424', '77777777', '343434343', '4444444', '77343434', '4443211'];
+array.filter(number => ['2', '4'].includes(number[0])).filter(print)
 
+// Не понял, почему перебор массива конфликует с функцией обрезки текста
+
+
+const cutText = (text) => {
+    if (typeof text === 'string') {
+        text = text.trim();
+        if (text.length > 30) {
+            text = text.slice(30).concat('...');
+        }
+        return text;
+
+    } else console.log('Это не текст!');
+
+}
+ const cuttingResult = cutText('sdasda ad asd ads asd asd asd asd asd asd222 2 ')
+console.log(cuttingResult)
