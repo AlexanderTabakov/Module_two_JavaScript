583b1fe5fd823899953aaa3a54e1a3cb2981b41e
AlexanderTabakov <sir.alexander.tabakov@gmail.com>
2023-03-22 12:17:17 +0700

    Сделал ДЗ 10

--- scripts/script.js
+++ scripts/script.js
@@ -1,27 +1,42 @@
 "use strict";
 
-const money = 100000;
+const getUsers = () => {
+    const url = 'https://reqres.in/api/users?per_page=12';
 
-const profit = 'Фриланс';
+    return fetch(url)
+    .then(response => response.json())
+}
 
-const expenses = 'Коммуналка, Питание, Бензин, Котик, Бары '
+const printSurnames = (users) => {
+    users.forEach(({last_name: surname}) => console.log(surname))
+    return users
 
-const purpose = 1000000;
+}
 
-const period = 3;
+const printUserWithSurnameStartsWithF = (users) => {
+    users.filter(({last_name: surname}) => surname.startsWith('F'))
+        .forEach(users => console.log(users))
+    return users
 
-console.log(typeof money, typeof profit);
+}
 
-console.log(expenses.length);
+const printDataBase = (users) => {
+  const result =  users.map(({first_name: name, last_name: surname}) => `${surname} ${name}`)
+       .join(', ')
+    console.log(`Наша база содержит данные следующих польщователей: ${result}`)
 
-console.log(`Период равен ${period}--м месяцем`);
+    return users
+}
 
-console.log(`Цель заработать ${purpose} рублей`);
+const printKeys = (user) => {
+    Object.keys(user).forEach(key => console.log(key))
 
-const budgetPerDay = money / 30;
 
-console.log(`${ Math.floor(budgetPerDay)}р / в день`);
+}
 
-console.log(expenses.toLowerCase());
-
-console.log(expenses.split(`, `));
+getUsers()
+.then(({data: users}) => users)
+.then(printSurnames)
+.then(printUserWithSurnameStartsWithF)
+.then(printDataBase)
+.then(printKeys)
